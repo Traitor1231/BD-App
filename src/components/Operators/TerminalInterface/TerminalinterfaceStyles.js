2f8deaa6941b87eb9115ddc1912ec7643e3ffb06
Ne kit <nikita234mail.ru@mail.ru>
2020-05-17 18:07:01 +0400

    + another encapsulating styles

---
+++ src/components/Operators/TerminalInterface/TerminalinterfaceStyles.js
@@ -0,0 +1,94 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+display: flex;
+flex-direction: column;
+justify-content: center;
+margin: 0px 10px;
+`
+export const PayButton = styled.button`
+ width:50%;
+ padding: 10px 2vw;
+ font-size:4vw;
+ color: white;
+ border-radius: 500px 0px 0px 500px;
+ background: none;
+ border: 2px solid white;
+ margin: 15px 0px;
+ transition:0.3s;
+ cursor:pointer;
+ outline:none;
+ :hover{
+     transform: scale(1.1);
+     background: white;
+     border: 2px solid black;
+     color:black;
+}
+ @media(max-width:357px){
+     font-size: 14px;
+}
+
+`
+export const BackButton = styled(PayButton)`
+    width:50%;
+    border-radius: 0px 500px 500px 0px;
+`
+
+export const Input = styled.input`
+   
+ -moz-appearance:textfield;
+ width: 100%;
+ padding: 10px 2vw;
+ font-size:4vw;
+ color: white;
+ border-radius: 500px;
+ background: none;
+ border: 2px solid white;
+ margin: 15px 0px;
+ transition:0.3s;
+ outline:none;
+ ::-ms-clear {
+     display: none;
+}
+ ::placeholder{
+     opacity: 1;
+     color:white;
+     font-size:4vw;
+     @media(max-width:357px){
+         font-size: 14px;
+    }
+}
+ :-ms-input-placeholder {
+     color: white;
+}
+ ::-ms-input-placeholder {
+     color: white;
+}
+ :invalid {
+     box-shadow: none;
+}
+ :-moz-submit-invalid {
+     box-shadow: none;
+}
+ :-moz-ui-invalid {
+     box-shadow:none;
+}
+ :hover,:focus{
+     transform: scale(1.05);
+     color:black;
+     background:white;
+     border: 2px solid black;
+     ::placeholder{
+         color:black;
+    }
+     ::-webkit-inner-spin-button {
+         -webkit-appearance: none;
+         margin: 0;
+    }
+     @media(max-width:357px){
+         font-size: 14px;
+    }
+     
+  `
+export const InputWrapper = styled.div`
+`
